5a5ea417d21e9d1ed109941c98af72b26fed2d76
maryory10 <145937337+maryory10@users.noreply.github.com>
2025-04-11 22:00:13 -0600

    Terminados los filtros de docente- grado- materia- alumnos por cada una con sus vistas

--- ITCA-Plus/Controllers/notasController.cs
+++ ITCA-Plus/Controllers/notasController.cs
@@ -1,10 +1,12 @@
 ﻿using ITCA_Plus.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ITCA_Plus.Controllers
 {
@@ -12,57 +14,96 @@ namespace ITCA_Plus.Controllers
     {
         ITCAPlusEntities contexto = new ITCAPlusEntities();
         int userActualID = 1;
-        public void llenarCmb()
+        public void llenarCmb(int flag=0)
         {
-            //Llenado de cmb para filtrar
-            //cmbAlumnos
-            ViewBag.cmbAlumnos = contexto.Alumno.Select(x => new SelectListItem
+            if(flag > 0)
             {
-                Text = x.nombre,
-                Value = x.id.ToString(),
-            });
-            //cmbMateria
-            ViewBag.cmbMateria = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID)
+                //cmbGrado
+                ViewBag.cmbGrado = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID)
+                    .Select(x => new SelectListItem
+                    {
+                        Text = x.grado_nombre,
+                        Value = x.grado_id.ToString()
+                    }).Distinct()
+                    .ToList();
+            }
+            else
+            {
+                //cmbGrado
+                ViewBag.cmbGrado = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID)
+                    .Select(x => new SelectListItem
+                    {
+                        Text = x.grado_nombre,
+                        Value = x.grado_id.ToString()
+                    }).Distinct()
+                    .ToList();
+                //View Por default
+             ViewBag.cmbMateria = new List<SelectListItem>
+            {
+               new SelectListItem { Text = "1", Value="1"},
+               new SelectListItem { Text = "1", Value="1"}
+            };
+             ViewBag.cmbAlumnos = new List<SelectListItem>
+            {
+               new SelectListItem { Text = "1", Value="1"},
+               new SelectListItem { Text = "1", Value="1"}
+             };
+            }
+            
+
+        }
+        [HttpPost]
+        public JsonResult ObtenerAlumnosPorGrado(int grado,int mat)
+        {
+            var alumnos =contexto.vw_AlumnosPorMateriaGrado.Where(x => x.docente_id == userActualID
+                && x.materia_id == mat && x.grado_id == grado).Select(x => new SelectListItem
+                {
+                    Text = x.alumno,
+                    Value = x.alumno_id.ToString(),
+                }).ToList();
+
+            return Json(alumnos, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult ObtenerMaterias(int grado)
+        {
+            var materias = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID
+            && x.grado_id == grado) 
                 .Select(x => new SelectListItem
                 {
-                    Text = x.materia_nombre, 
+                    Text = x.materia_nombre,
                     Value = x.materia_id.ToString()
-                }).Distinct() 
-                .ToList();
-            //cmbGrado
-            ViewBag.cmbGrado = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID)
-                .Select(x => new SelectListItem
-                {
-                    Text = x.grado_nombre,
-                    Value = x.grado_id.ToString()
                 }).Distinct()
                 .ToList();
-            //cmbseccion
-            ViewBag.cmbSeccion = contexto.Grado.Select(x => new SelectListItem
-            {
-                Text = x.seccion,
-                Value = x.seccion.ToString(),
-            });
-            
+
+            return Json(materias, JsonRequestBehavior.AllowGet);
         }
-        
+
         // GET: notas
         public ActionResult CuadroCalificaciones()
         {
-            llenarCmb();
+             llenarCmb();
             return View();
         }
         [HttpPost]
-        public ActionResult CuadroCalificaciones(int grado, int materia)
+        public ActionResult CuadroCalificaciones(int grado, int materia, int alumno=0)
         {
             if (ModelState.IsValid)
             {
-                var notas = contexto.vw_AlumnosPorMateriaGrado.Where(n => n.grado_id == grado
-                && n.materia_id == materia).ToList();
-
-                ViewBag.NotasFiltradas = notas;
+                if (alumno != 0)
+                {
+                  var notas = contexto.vw_AlumnosPorMateriaGrado.Where(n => n.grado_id == grado
+                        && n.materia_id == materia && n.alumno_id == alumno).ToList();
+                    ViewBag.NotasFiltradas = notas;
+                }
+                else
+                {
+                    var notas = contexto.vw_AlumnosPorMateriaGrado.Where(n => n.grado_id == grado
+                        && n.materia_id == materia).ToList();
+                    ViewBag.NotasFiltradas = notas;
+                }
             }
-            llenarCmb();
+            llenarCmb(1);
             return View();
         }
 
@@ -73,16 +114,16 @@ namespace ITCA_Plus.Controllers
             ViewBag.data = data;
             return View();
         }
-        [HttpPost]  
-        public ActionResult CertificadoNotas(notasEstudiantes notas)
+        /*[HttpPost]  
+        public ActionResult CertificadoNotas()
         {
             if (ModelState.IsValid)
             {
                 // Procesar la lógica de guardado
                 // Redirigir a otra acción o devolver una vista
             }
-            return View(notas);
-        }
+            return View();
+        }*/
     }
 
 }
