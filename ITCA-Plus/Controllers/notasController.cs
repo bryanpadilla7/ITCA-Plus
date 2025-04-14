249a58d505b734a002087ca907bc21313a8a08c7
maryory10 <145937337+maryory10@users.noreply.github.com>
2025-04-13 18:09:40 -0600

    Terminado el AgregarNotas -- filtra por grado y materia -- filtra por grado, materia y alumno especifico -- guarda notas atraves de un modal -- desactiva el boton agregar notas, cuando ya se encuentran guardadas las notas para ese trimestre Comienzo de la funcion editar -- se guarda la variable si es editar o guardar -- estilo del modal destinado a mandar la peticion al admin de un cambio -- modificada la parte de habilitar cuando ya posee el permiso del admin

--- ITCA-Plus/Controllers/notasController.cs
+++ ITCA-Plus/Controllers/notasController.cs
@@ -14,21 +14,35 @@ namespace ITCA_Plus.Controllers
     {
         ITCAPlusEntities contexto = new ITCAPlusEntities();
         int userActualID = 1;
-        public void llenarCmb()
+        public void llenarCmb(int cmb)
         {
-                //cmbGrado
-                ViewBag.cmbGrado = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID)
-                    .Select(x => new SelectListItem
-                    {
-                        Text = x.grado_nombre,
-                        Value = x.grado_id.ToString()
-                    }).Distinct()
-                    .ToList();
+            switch (cmb)
+            {
+                case 1:
+                    //cmbGrado
+                    ViewBag.cmbGrado = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID)
+                        .Select(x => new SelectListItem
+                        {
+                            Text = x.grado_nombre,
+                            Value = x.grado_id.ToString()
+                        }).Distinct()
+                        .ToList();
+                    break;
+                case 2:
+                    ViewBag.trimestres = new List<SelectListItem>
+                {
+                   new SelectListItem { Text = "Primer Trimestre", Value="Primer Trimestre"},
+                   new SelectListItem { Text = "Segundo Trimestre", Value="Segundo Trimestre"},
+                   new SelectListItem { Text = "Tercer Trimestre", Value="Tercer Trimestre"},
+                };
+                    break;
+            }
+            
         }
         [HttpPost]
-        public JsonResult ObtenerAlumnosPorGrado(int grado,int mat)
+        public JsonResult ObtenerAlumnosPorGrado(int grado, int mat)
         {
-            var alumnos =contexto.vw_AlumnosPorMateriaGrado.Where(x => x.docente_id == userActualID
+            var alumnos = contexto.vw_AlumnosPorMateriaGrado.Where(x => x.docente_id == userActualID
                 && x.materia_id == mat && x.grado_id == grado).Select(x => new SelectListItem
                 {
                     Text = x.alumno,
@@ -41,7 +55,7 @@ namespace ITCA_Plus.Controllers
         public JsonResult ObtenerMaterias(int grado)
         {
             var materias = contexto.vw_MateriasAsignadasDocente.Where(x => x.docente_id == userActualID
-            && x.grado_id == grado) 
+            && x.grado_id == grado)
                 .Select(x => new SelectListItem
                 {
                     Text = x.materia_nombre,
@@ -55,33 +69,85 @@ namespace ITCA_Plus.Controllers
         // GET: notas
         public ActionResult CuadroCalificaciones()
         {
-             llenarCmb();
+            llenarCmb(1);
             return View();
         }
         [HttpPost]
-        public ActionResult CuadroCalificaciones(int grado, int materia, int alumno=0)
+        public ActionResult CuadroCalificaciones(int grado, int materia, int alumno = 0)
         {
             if (ModelState.IsValid)
             {
-                ModelState.Remove("grado");
-                llenarCmb();
-                if (alumno != 0)
-                {
-                  var notas = contexto.vw_AlumnosPorMateriaGrado.Where(n => n.grado_id == grado
-                        && n.materia_id == materia && n.alumno_id == alumno).ToList();
-                    ViewBag.ListadoPorMateriaGrado = notas;
-                }
-                else
-                {
-                    var notas = contexto.vw_AlumnosPorMateriaGrado.Where(n => n.grado_id == grado
-                        && n.materia_id == materia).ToList();
-                    ViewBag.ListadoPorMateriaGrado = notas;
-                }
+                /*Aqui debo de agregar la sentencia de buscar en la tabla NotasEditar si ya me dieron permiso de edicion o no
+                ViewBag.permisoEdicion = contexto.NotasEditar.Where(x => x.permiso == 1)
+                */
+                ViewBag.NotasYaGuadadas = contexto.Notas.Where(x => x.trimestres == "Primer Trimestre" 
+                || x.trimestres == "Segundo Trimestre" || x.trimestres == "Tercer Trimestre").ToList();
+                    ModelState.Remove("grado");
+                    llenarCmb(1);
+                    if (alumno != 0)
+                    {
+                        var notas = contexto.vw_AlumnosPorMateriaGrado.Where(n => n.grado_id == grado
+                              && n.materia_id == materia && n.alumno_id == alumno).ToList();
+                        ViewBag.ListadoPorMateriaGrado = notas;
+                    }
+                    else
+                    {
+                        var notas = contexto.vw_AlumnosPorMateriaGrado.Where(n => n.grado_id == grado
+                            && n.materia_id == materia).ToList();
+                        ViewBag.ListadoPorMateriaGrado = notas;
+                    }
             }
-            
+
             return View();
         }
+        [HttpPost]
+        public JsonResult  IngresoNotas(Notas n)
+        {
+            string accion = Request.Form["boton"].ToString();
+            string resultado = "";
+            switch (accion)
+            {
+                case "Guardar":
+                    contexto.Notas.Add(n);
+                    contexto.SaveChanges();
+                    resultado = "Guardadas";
+                    break;
+                case "Editar":
+                    Notas temp = contexto.Notas.FirstOrDefault(x => x.alumno_id == n.alumno_id && x.materia_id == n.materia_id && x.anio_escolar_id == n.anio_escolar_id);
+                    temp.alumno_id = n.alumno_id;
+                    temp.materia_id = n.materia_id;
+                    temp.anio_escolar_id = n.anio_escolar_id;
+                    temp.nota1 = n.nota1;
+                    temp.nota2 = n.nota2;
+                    temp.nota3 = n.nota3;
+                    contexto.SaveChanges();
+                    resultado = "Modificadas";
+                    break;
+            }
+            
+            return Json(resultado);
+        }
+        [HttpPost]
+        public JsonResult NotasGuardadas(int materia, int alumno, string trimestre)
+        {
+            List<Notas> data = contexto.Notas.Where(x => x.alumno_id == alumno && x.materia_id == materia && x.trimestres == trimestre).ToList();
+            ViewBag.data = data;
+            string resultado = "";
+            if (data.Count() > 0)
+            {
+                 resultado = "Existe";
+            }
+           
+            return Json(resultado);
+        }
 
+        [HttpPost]
+        public JsonResult EditarNotas()
+        {
+
+            string resultado = "";
+            return Json(resultado);
+        }
         public ActionResult CertificadoNotas()
         {
             List<Alumno> data = contexto.Alumno.ToList();
