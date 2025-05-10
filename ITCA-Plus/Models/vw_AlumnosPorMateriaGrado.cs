3f2bac50985da14cdf346939fbd270794f854114
Luis Enrique Elías Gómez <149125562+luishi22@users.noreply.github.com>
2025-05-09 21:00:48 -0600

    Revert "Merge remote-tracking branch 'origin/Bryan2' into MergeMaster"
    This reverts commit 18de502a8084bcee1cf26b48a3f320451e22c9fc, reversing
    changes made to 9a3a9c4463da034ceca852efcd266e409a5d4273.

--- ITCA-Plus/Models/vw_AlumnosPorMateriaGrado.cs
+++ ITCA-Plus/Models/vw_AlumnosPorMateriaGrado.cs
@@ -23,6 +23,5 @@ namespace ITCA_Plus.Models
         public string alumno { get; set; }
         public string grado { get; set; }
         public string materia { get; set; }
-        public Nullable<int> anio_escolar { get; set; }
     }
 }
